cec3eb676b93c8fa426e2aacabe0124c918f89cf
Paul Kermann <99318815+paulkermann@users.noreply.github.com>
2022-05-03 17:20:53 +0300

    check 0.975

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError as excp:
 
 
 setup(name='minidumpwriter',
-      version='0.971',
+      version='0.975',
       description='Minidump writer',
       author='Paul Kermann',
       author_email='paulkermann@tutanota.com',
