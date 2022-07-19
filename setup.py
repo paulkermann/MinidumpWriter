8859787ec29efdbb1d263789a43131f4093cbe20
Paul Kermann <paulkermann@tutanota.com>
2022-07-19 16:38:11 +0300

    added specific long description

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ try:
 except ImportError as excp:
     from distutils.core import setup, Command
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='minidumpwriter',
       version='0.98',
       description='Minidump writer',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Paul Kermann',
       author_email='paulkermann@tutanota.com',
       url='https://github.com/paulkermann/MinidumpWriter',
