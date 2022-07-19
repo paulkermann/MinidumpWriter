daf9750264b10b00636c46aa38da23cee25ec756
Paul Kermann <paulkermann@tutanota.com>
2022-07-19 16:47:01 +0300

    support python2 setup

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os
 try:
     from setuptools import setup, Command
 except ImportError as excp:
     from distutils.core import setup, Command
 
-from pathlib import Path
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+except Exception:
+    long_description = ''
+    pass
 
 setup(name='minidumpwriter',
       version='0.98',
